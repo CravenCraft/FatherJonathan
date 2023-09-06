b037550bd7da3137b0fc6fb0f9e0bfb511ae93e8
ryan-craven <benjamin.r.craven@gmail.com>
2023-09-05 22:54:20 -0400

    Restructures texture directories, added extra textures

--- overrides/kubejs/startup_scripts/PapaJohnsItems.js
+++ overrides/kubejs/startup_scripts/PapaJohnsItems.js
@@ -12,16 +12,29 @@ StartupEvents.registry('item', e => {
 
   // e.create('pickaxe_head')
 
+  // Axe
   e.create('axe_head_cast').texture('molten_metals:item/parts/axe_head/cast')
   e.create('axe_head_cast_iron').texture('molten_metals:item/parts/axe_head/iron/cast')
   e.create('axe_head_cast_diamond').texture('molten_metals:item/parts/axe_head/diamond/cast')
   e.create('axe_head_cast_netherite').texture('molten_metals:item/parts/axe_head/netherite/cast')
   e.create('axe_head_cast_runic').texture('molten_metals:item/parts/axe_head/runic/cast')
 
-  e.create('axe_head_iron').texture('molten_metals:item/parts/axe_head/iron/axe_head')
-  e.create('axe_head_diamond').texture('molten_metals:item/parts/axe_head/diamond/axe_head')
-  e.create('axe_head_netherite').texture('molten_metals:item/parts/axe_head/netherite/axe_head')
-  e.create('axe_head_runic').texture('molten_metals:item/parts/axe_head/runic/axe_head')
+  e.create('axe_head_iron').texture('molten_metals:item/parts/axe_head/iron/head')
+  e.create('axe_head_diamond').texture('molten_metals:item/parts/axe_head/diamond/head')
+  e.create('axe_head_netherite').texture('molten_metals:item/parts/axe_head/netherite/head')
+  e.create('axe_head_runic').texture('molten_metals:item/parts/axe_head/runic/head')
+
+  // Pickaxe
+  e.create('pickaxe_head_cast').texture('molten_metals:item/parts/pickaxe_head/cast')
+  e.create('pickaxe_head_cast_iron').texture('molten_metals:item/parts/pickaxe_head/iron/cast')
+  e.create('pickaxe_head_cast_diamond').texture('molten_metals:item/parts/pickaxe_head/diamond/cast')
+  e.create('pickaxe_head_cast_netherite').texture('molten_metals:item/parts/pickaxe_head/netherite/cast')
+  e.create('pickaxe_head_cast_runic').texture('molten_metals:item/parts/pickaxe_head/runic/cast')
+  
+  e.create('pickaxe_head_iron').texture('molten_metals:item/parts/pickaxe_head/iron/head')
+  e.create('pickaxe_head_diamond').texture('molten_metals:item/parts/pickaxe_head/diamond/head')
+  e.create('pickaxe_head_netherite').texture('molten_metals:item/parts/pickaxe_head/netherite/head')
+  e.create('pickaxe_head_runic').texture('molten_metals:item/parts/pickaxe_head/runic/head')
 
   // e.create('broad_axe_head')
   // e.create('broad_axe_head_gold')
@@ -33,6 +46,20 @@ StartupEvents.registry('item', e => {
   // e.create('broad_blade')
   // e.create('small_blade')
 
+  // Chakram Blade
+  e.create('chakram_blade_cast').texture('molten_metals:item/parts/chakram_blade/cast')
+  e.create('chakram_blade_cast_iron').texture('molten_metals:item/parts/chakram_blade/iron/cast')
+  e.create('chakram_blade_cast_diamond').texture('molten_metals:item/parts/chakram_blade/diamond/cast')
+  e.create('chakram_blade_cast_netherite').texture('molten_metals:item/parts/chakram_blade/netherite/cast')
+  e.create('chakram_blade_cast_runic').texture('molten_metals:item/parts/chakram_blade/runic/cast')
+
+  e.create('chakram_blade_iron').texture('molten_metals:item/parts/chakram_blade/iron/blade')
+  e.create('chakram_blade_diamond').texture('molten_metals:item/parts/chakram_blade/diamond/blade')
+  e.create('chakram_blade_netherite').texture('molten_metals:item/parts/chakram_blade/netherite/blade')
+  e.create('chakram_blade_runic').texture('molten_metals:item/parts/chakram_blade/runic/blade')
+
+
+  // Double Sided Medium Blade
   e.create('double_sided_blade_medium_cast').texture('molten_metals:item/parts/double_sided_blade_medium/cast')
   e.create('double_sided_blade_medium_cast_iron').texture('molten_metals:item/parts/double_sided_blade_medium/iron/cast')
   e.create('double_sided_blade_medium_cast_diamond').texture('molten_metals:item/parts/double_sided_blade_medium/diamond/cast')
@@ -43,7 +70,18 @@ StartupEvents.registry('item', e => {
   e.create('double_sided_blade_medium_diamond').texture('molten_metals:item/parts/double_sided_blade_medium/diamond/blade')
   e.create('double_sided_blade_medium_netherite').texture('molten_metals:item/parts/double_sided_blade_medium/netherite/blade')
   e.create('double_sided_blade_medium_runic').texture('molten_metals:item/parts/double_sided_blade_medium/runic/blade')
-  
+
+  // Double Sided Large Blade
+  e.create('double_sided_blade_large_cast').texture('molten_metals:item/parts/double_sided_blade_large/cast')
+  e.create('double_sided_blade_large_cast_iron').texture('molten_metals:item/parts/double_sided_blade_large/iron/cast')
+  e.create('double_sided_blade_large_cast_diamond').texture('molten_metals:item/parts/double_sided_blade_large/diamond/cast')
+  e.create('double_sided_blade_large_cast_netherite').texture('molten_metals:item/parts/double_sided_blade_large/netherite/cast')
+  e.create('double_sided_blade_large_cast_runic').texture('molten_metals:item/parts/double_sided_blade_large/runic/cast')
+
+  e.create('double_sided_blade_large_iron').texture('molten_metals:item/parts/double_sided_blade_large/iron/blade')
+  e.create('double_sided_blade_large_diamond').texture('molten_metals:item/parts/double_sided_blade_large/diamond/blade')
+  e.create('double_sided_blade_large_netherite').texture('molten_metals:item/parts/double_sided_blade_large/netherite/blade')
+  e.create('double_sided_blade_large_runic').texture('molten_metals:item/parts/double_sided_blade_large/runic/blade')
 
   // e.create('hammer_head')
   // e.create('rod')
