362b653680bca9029ff989fea3fb1405e1c0a4bc
ryan-craven <benjamin.r.craven@gmail.com>
2023-09-28 23:00:54 -0400

    Finished all molten metal parts

--- overrides/kubejs/startup_scripts/PapaJohnsItems.js
+++ overrides/kubejs/startup_scripts/PapaJohnsItems.js
@@ -10,8 +10,6 @@ StartupEvents.registry('item', e => {
   // e.create('tough_handle')
   // e.create('sheet')
 
-  // e.create('pickaxe_head')
-
   // Axe
   e.create('axe_head_cast').texture('molten_metals:item/parts/axe_head/cast')
   e.create('axe_head_cast_iron').texture('molten_metals:item/parts/axe_head/iron/cast')
@@ -23,28 +21,6 @@ StartupEvents.registry('item', e => {
   e.create('axe_head_diamond').texture('molten_metals:item/parts/axe_head/diamond/head')
   e.create('axe_head_netherite').texture('molten_metals:item/parts/axe_head/netherite/head')
   e.create('axe_head_runic').texture('molten_metals:item/parts/axe_head/runic/head')
-
-  // Pickaxe
-  e.create('pickaxe_head_cast').texture('molten_metals:item/parts/pickaxe_head/cast')
-  e.create('pickaxe_head_cast_iron').texture('molten_metals:item/parts/pickaxe_head/iron/cast')
-  e.create('pickaxe_head_cast_diamond').texture('molten_metals:item/parts/pickaxe_head/diamond/cast')
-  e.create('pickaxe_head_cast_netherite').texture('molten_metals:item/parts/pickaxe_head/netherite/cast')
-  e.create('pickaxe_head_cast_runic').texture('molten_metals:item/parts/pickaxe_head/runic/cast')
-  
-  e.create('pickaxe_head_iron').texture('molten_metals:item/parts/pickaxe_head/iron/head')
-  e.create('pickaxe_head_diamond').texture('molten_metals:item/parts/pickaxe_head/diamond/head')
-  e.create('pickaxe_head_netherite').texture('molten_metals:item/parts/pickaxe_head/netherite/head')
-  e.create('pickaxe_head_runic').texture('molten_metals:item/parts/pickaxe_head/runic/head')
-
-  // e.create('broad_axe_head')
-  // e.create('broad_axe_head_gold')
-  // e.create('broad_axe_head_iron')
-  // e.create('broad_axe_head_diamond')
-  // e.create('broad_axe_head_netherite')
-  // e.create('broad_axe_head_runic')
-
-  // e.create('broad_blade')
-  // e.create('small_blade')
 
   // Chakram Blade
   e.create('chakram_blade_cast').texture('molten_metals:item/parts/chakram_blade/cast')
@@ -58,6 +34,17 @@ StartupEvents.registry('item', e => {
   e.create('chakram_blade_netherite').texture('molten_metals:item/parts/chakram_blade/netherite/blade')
   e.create('chakram_blade_runic').texture('molten_metals:item/parts/chakram_blade/runic/blade')
 
+  // Double Sided Large Blade
+  e.create('double_sided_blade_large_cast').texture('molten_metals:item/parts/double_sided_blade_large/cast')
+  e.create('double_sided_blade_large_cast_iron').texture('molten_metals:item/parts/double_sided_blade_large/iron/cast')
+  e.create('double_sided_blade_large_cast_diamond').texture('molten_metals:item/parts/double_sided_blade_large/diamond/cast')
+  e.create('double_sided_blade_large_cast_netherite').texture('molten_metals:item/parts/double_sided_blade_large/netherite/cast')
+  e.create('double_sided_blade_large_cast_runic').texture('molten_metals:item/parts/double_sided_blade_large/runic/cast')
+
+  e.create('double_sided_blade_large_iron').texture('molten_metals:item/parts/double_sided_blade_large/iron/blade')
+  e.create('double_sided_blade_large_diamond').texture('molten_metals:item/parts/double_sided_blade_large/diamond/blade')
+  e.create('double_sided_blade_large_netherite').texture('molten_metals:item/parts/double_sided_blade_large/netherite/blade')
+  e.create('double_sided_blade_large_runic').texture('molten_metals:item/parts/double_sided_blade_large/runic/blade')
 
   // Double Sided Medium Blade
   e.create('double_sided_blade_medium_cast').texture('molten_metals:item/parts/double_sided_blade_medium/cast')
@@ -71,22 +58,114 @@ StartupEvents.registry('item', e => {
   e.create('double_sided_blade_medium_netherite').texture('molten_metals:item/parts/double_sided_blade_medium/netherite/blade')
   e.create('double_sided_blade_medium_runic').texture('molten_metals:item/parts/double_sided_blade_medium/runic/blade')
 
-  // Double Sided Large Blade
-  e.create('double_sided_blade_large_cast').texture('molten_metals:item/parts/double_sided_blade_large/cast')
-  e.create('double_sided_blade_large_cast_iron').texture('molten_metals:item/parts/double_sided_blade_large/iron/cast')
-  e.create('double_sided_blade_large_cast_diamond').texture('molten_metals:item/parts/double_sided_blade_large/diamond/cast')
-  e.create('double_sided_blade_large_cast_netherite').texture('molten_metals:item/parts/double_sided_blade_large/netherite/cast')
-  e.create('double_sided_blade_large_cast_runic').texture('molten_metals:item/parts/double_sided_blade_large/runic/cast')
+  // Double Sided Small Blade
+  e.create('double_sided_blade_small_cast').texture('molten_metals:item/parts/double_sided_blade_small/cast')
+  e.create('double_sided_blade_small_cast_iron').texture('molten_metals:item/parts/double_sided_blade_small/iron/cast')
+  e.create('double_sided_blade_small_cast_diamond').texture('molten_metals:item/parts/double_sided_blade_small/diamond/cast')
+  e.create('double_sided_blade_small_cast_netherite').texture('molten_metals:item/parts/double_sided_blade_small/netherite/cast')
+  e.create('double_sided_blade_small_cast_runic').texture('molten_metals:item/parts/double_sided_blade_small/runic/cast')
 
-  e.create('double_sided_blade_large_iron').texture('molten_metals:item/parts/double_sided_blade_large/iron/blade')
-  e.create('double_sided_blade_large_diamond').texture('molten_metals:item/parts/double_sided_blade_large/diamond/blade')
-  e.create('double_sided_blade_large_netherite').texture('molten_metals:item/parts/double_sided_blade_large/netherite/blade')
-  e.create('double_sided_blade_large_runic').texture('molten_metals:item/parts/double_sided_blade_large/runic/blade')
+  e.create('double_sided_blade_small_iron').texture('molten_metals:item/parts/double_sided_blade_small/iron/blade')
+  e.create('double_sided_blade_small_diamond').texture('molten_metals:item/parts/double_sided_blade_small/diamond/blade')
+  e.create('double_sided_blade_small_netherite').texture('molten_metals:item/parts/double_sided_blade_small/netherite/blade')
+  e.create('double_sided_blade_small_runic').texture('molten_metals:item/parts/double_sided_blade_small/runic/blade')  
 
-  // e.create('hammer_head')
-  // e.create('rod')
+  // Hammer Head
+  e.create('hammer_head_cast').texture('molten_metals:item/parts/hammer_head/cast')
+  e.create('hammer_head_cast_iron').texture('molten_metals:item/parts/hammer_head/iron/cast')
+  e.create('hammer_head_cast_diamond').texture('molten_metals:item/parts/hammer_head/diamond/cast')
+  e.create('hammer_head_cast_netherite').texture('molten_metals:item/parts/hammer_head/netherite/cast')
+  e.create('hammer_head_cast_runic').texture('molten_metals:item/parts/hammer_head/runic/cast')
 
+  e.create('hammer_head_iron').texture('molten_metals:item/parts/hammer_head/iron/head')
+  e.create('hammer_head_diamond').texture('molten_metals:item/parts/hammer_head/diamond/head')
+  e.create('hammer_head_netherite').texture('molten_metals:item/parts/hammer_head/netherite/head')
+  e.create('hammer_head_runic').texture('molten_metals:item/parts/hammer_head/runic/head')
+
+  // Pickaxe Head
+  e.create('pickaxe_head_cast').texture('molten_metals:item/parts/pickaxe_head/cast')
+  e.create('pickaxe_head_cast_iron').texture('molten_metals:item/parts/pickaxe_head/iron/cast')
+  e.create('pickaxe_head_cast_diamond').texture('molten_metals:item/parts/pickaxe_head/diamond/cast')
+  e.create('pickaxe_head_cast_netherite').texture('molten_metals:item/parts/pickaxe_head/netherite/cast')
+  e.create('pickaxe_head_cast_runic').texture('molten_metals:item/parts/pickaxe_head/runic/cast')
   
+  e.create('pickaxe_head_iron').texture('molten_metals:item/parts/pickaxe_head/iron/head')
+  e.create('pickaxe_head_diamond').texture('molten_metals:item/parts/pickaxe_head/diamond/head')
+  e.create('pickaxe_head_netherite').texture('molten_metals:item/parts/pickaxe_head/netherite/head')
+  e.create('pickaxe_head_runic').texture('molten_metals:item/parts/pickaxe_head/runic/head')
+
+  // Shovel Head
+  e.create('shovel_head_cast').texture('molten_metals:item/parts/shovel_head/cast')
+  e.create('shovel_head_cast_iron').texture('molten_metals:item/parts/shovel_head/iron/cast')
+  e.create('shovel_head_cast_diamond').texture('molten_metals:item/parts/shovel_head/diamond/cast')
+  e.create('shovel_head_cast_netherite').texture('molten_metals:item/parts/shovel_head/netherite/cast')
+  e.create('shovel_head_cast_runic').texture('molten_metals:item/parts/shovel_head/runic/cast')
+  
+  e.create('shovel_head_iron').texture('molten_metals:item/parts/shovel_head/iron/head')
+  e.create('shovel_head_diamond').texture('molten_metals:item/parts/shovel_head/diamond/head')
+  e.create('shovel_head_netherite').texture('molten_metals:item/parts/shovel_head/netherite/head')
+  e.create('shovel_head_runic').texture('molten_metals:item/parts/shovel_head/runic/head')
+
+  // Single Sided Large Blade
+  e.create('single_sided_blade_large_cast').texture('molten_metals:item/parts/single_sided_blade_large/cast')
+  e.create('single_sided_blade_large_cast_iron').texture('molten_metals:item/parts/single_sided_blade_large/iron/cast')
+  e.create('single_sided_blade_large_cast_diamond').texture('molten_metals:item/parts/single_sided_blade_large/diamond/cast')
+  e.create('single_sided_blade_large_cast_netherite').texture('molten_metals:item/parts/single_sided_blade_large/netherite/cast')
+  e.create('single_sided_blade_large_cast_runic').texture('molten_metals:item/parts/single_sided_blade_large/runic/cast')
+
+  e.create('single_sided_blade_large_iron').texture('molten_metals:item/parts/single_sided_blade_large/iron/blade')
+  e.create('single_sided_blade_large_diamond').texture('molten_metals:item/parts/single_sided_blade_large/diamond/blade')
+  e.create('single_sided_blade_large_netherite').texture('molten_metals:item/parts/single_sided_blade_large/netherite/blade')
+  e.create('single_sided_blade_large_runic').texture('molten_metals:item/parts/single_sided_blade_large/runic/blade') 
+
+  // Single Sided Medium Blade
+  e.create('single_sided_blade_medium_cast').texture('molten_metals:item/parts/single_sided_blade_medium/cast')
+  e.create('single_sided_blade_medium_cast_iron').texture('molten_metals:item/parts/single_sided_blade_medium/iron/cast')
+  e.create('single_sided_blade_medium_cast_diamond').texture('molten_metals:item/parts/single_sided_blade_medium/diamond/cast')
+  e.create('single_sided_blade_medium_cast_netherite').texture('molten_metals:item/parts/single_sided_blade_medium/netherite/cast')
+  e.create('single_sided_blade_medium_cast_runic').texture('molten_metals:item/parts/single_sided_blade_medium/runic/cast')
+
+  e.create('single_sided_blade_medium_iron').texture('molten_metals:item/parts/single_sided_blade_medium/iron/blade')
+  e.create('single_sided_blade_medium_diamond').texture('molten_metals:item/parts/single_sided_blade_medium/diamond/blade')
+  e.create('single_sided_blade_medium_netherite').texture('molten_metals:item/parts/single_sided_blade_medium/netherite/blade')
+  e.create('single_sided_blade_medium_runic').texture('molten_metals:item/parts/single_sided_blade_medium/runic/blade')
+
+  // Single Sided Small Blade
+  e.create('single_sided_blade_small_cast').texture('molten_metals:item/parts/single_sided_blade_small/cast')
+  e.create('single_sided_blade_small_cast_iron').texture('molten_metals:item/parts/single_sided_blade_small/iron/cast')
+  e.create('single_sided_blade_small_cast_diamond').texture('molten_metals:item/parts/single_sided_blade_small/diamond/cast')
+  e.create('single_sided_blade_small_cast_netherite').texture('molten_metals:item/parts/single_sided_blade_small/netherite/cast')
+  e.create('single_sided_blade_small_cast_runic').texture('molten_metals:item/parts/single_sided_blade_small/runic/cast')
+
+  e.create('single_sided_blade_small_iron').texture('molten_metals:item/parts/single_sided_blade_small/iron/blade')
+  e.create('single_sided_blade_small_diamond').texture('molten_metals:item/parts/single_sided_blade_small/diamond/blade')
+  e.create('single_sided_blade_small_netherite').texture('molten_metals:item/parts/single_sided_blade_small/netherite/blade')
+  e.create('single_sided_blade_small_runic').texture('molten_metals:item/parts/single_sided_blade_small/runic/blade')
+
+  // Thin Medium Blade
+  e.create('thin_blade_medium_cast').texture('molten_metals:item/parts/thin_blade_medium/cast')
+  e.create('thin_blade_medium_cast_iron').texture('molten_metals:item/parts/thin_blade_medium/iron/cast')
+  e.create('thin_blade_medium_cast_diamond').texture('molten_metals:item/parts/thin_blade_medium/diamond/cast')
+  e.create('thin_blade_medium_cast_netherite').texture('molten_metals:item/parts/thin_blade_medium/netherite/cast')
+  e.create('thin_blade_medium_cast_runic').texture('molten_metals:item/parts/thin_blade_medium/runic/cast')
+
+  e.create('thin_blade_medium_iron').texture('molten_metals:item/parts/thin_blade_medium/iron/blade')
+  e.create('thin_blade_medium_diamond').texture('molten_metals:item/parts/thin_blade_medium/diamond/blade')
+  e.create('thin_blade_medium_netherite').texture('molten_metals:item/parts/thin_blade_medium/netherite/blade')
+  e.create('thin_blade_medium_runic').texture('molten_metals:item/parts/thin_blade_medium/runic/blade')
+
+  // Thin Small Blade
+  e.create('thin_blade_small_cast').texture('molten_metals:item/parts/thin_blade_small/cast')
+  e.create('thin_blade_small_cast_iron').texture('molten_metals:item/parts/thin_blade_small/iron/cast')
+  e.create('thin_blade_small_cast_diamond').texture('molten_metals:item/parts/thin_blade_small/diamond/cast')
+  e.create('thin_blade_small_cast_netherite').texture('molten_metals:item/parts/thin_blade_small/netherite/cast')
+  e.create('thin_blade_small_cast_runic').texture('molten_metals:item/parts/thin_blade_small/runic/cast')
+
+  e.create('thin_blade_small_iron').texture('molten_metals:item/parts/thin_blade_small/iron/blade')
+  e.create('thin_blade_small_diamond').texture('molten_metals:item/parts/thin_blade_small/diamond/blade')
+  e.create('thin_blade_small_netherite').texture('molten_metals:item/parts/thin_blade_small/netherite/blade')
+  e.create('thin_blade_small_runic').texture('molten_metals:item/parts/thin_blade_small/runic/blade')
+
   // If you want to specify a different texture location you can do that too, like this:
   // e.create('test_item_1').texture('mobbo:item/lava') // This texture would be located at kubejs/assets/mobbo/textures/item/lava.png
   
